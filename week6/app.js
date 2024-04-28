e964e40b136c3712831bc8aedf16776c1d042a58
suzzang2 <subin030102@naver.com>
2024-04-28 23:28:47 +0900

    Feat: add compAllBtn eventlistener

--- week6/app.js
+++ week6/app.js
@@ -2,6 +2,7 @@ import TodoController from "./controller/TodoController.js";
 
 const addBtn = document.getElementById('input');
 const input = document.querySelector('input');
+const compAllBtn = document.getElementById('compAllBtn');
 
 addBtn.addEventListener('click', () => { 
     if (input.value === '') return; //입력값이 없으면 목록 추가 안 함(예외처리)
@@ -9,3 +10,8 @@ addBtn.addEventListener('click', () => {
     const todoControl = new TodoController(input.value);
     todoControl.addTodo();
 })
+
+compAllBtn.addEventListener('click', () => {
+    console.log('compAllBtn');
+})
+
